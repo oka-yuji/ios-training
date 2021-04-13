c85302c4754043d8b7690efb13fb241e8c947172
岡優志 <yu.j.0513@gmail.com>
2021-04-13 22:27:23 +0900

    project作成

---
+++ IosTraining/IosTraining/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IosTraining
+//
+//  Created by 岡優志 on 2021/04/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IosTraining/IosTraining/IosTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosTrainingApp.swift
+//  IosTraining
+//
+//  Created by 岡優志 on 2021/04/13.
+//
+
+import SwiftUI
+
+@main
+struct IosTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
